a169e1ece59786616986c656ea627864f259efeb
Mary Davin <mary.davin@cit.ie>
2017-06-13 15:43:00 +0100

    made speculative changes

--- src/power.py
+++ src/power.py
@@ -1,10 +1,12 @@
+"This is a program
+"to calculate the power of a number "
 def to_the_power(x, y = 2):
-    result = x
-    for i in range(0, y):
-        result = result * x
-    return result
+#    result = x
+ #   for i in range(0, y):
+  #      result = result * x
+    return x ** y
 
 x = 9
-y = 8 #
+y = 8 
 print("{0} to the power of {1} is: {2} ", .format(x,y,to_the_power(x, y))
 print("The end")
