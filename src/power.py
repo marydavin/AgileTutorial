abf50f7dc4fe7d0019a1756d8e3c27bb7ac19de7
Mary Davin <mary.davin@cit.ie>
2017-06-13 16:02:21 +0100

    new square root function added

--- src/power.py
+++ src/power.py
@@ -5,8 +5,11 @@ def to_the_power(x, y = 2):
  #   for i in range(0, y):
   #      result = result * x
     return x ** y
+def square_root(x):
+    return math.sqrt(x)
 
 x = 9
 y = 8 
 print("{0} to the power of {1} is: {2} ", .format(x,y,to_the_power(x, y))
+print("The square root of {0} is {1}".format(x, square_root(x)))
 print("The end")
